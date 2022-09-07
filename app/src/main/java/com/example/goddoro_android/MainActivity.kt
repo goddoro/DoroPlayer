6d85ea923df7ceb5991aeb7b7fc9084efdf23aa9
doro-watcha <doro@watcha.com>
2022-09-07 23:45:17 +0900

    feat: ExoPlayer cache 모드

--- app/src/main/java/com/example/goddoro_android/MainActivity.kt
+++ app/src/main/java/com/example/goddoro_android/MainActivity.kt
@@ -1,28 +1,60 @@
 package com.example.goddoro_android
 
 import android.os.Bundle
-import android.support.v4.media.MediaBrowserCompat
 import android.view.LayoutInflater
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
 import com.example.goddoro_android.databinding.ActivityMainBinding
-import com.goddoro.player.view.PlayerView
+import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.database.DatabaseProvider
+import com.google.android.exoplayer2.database.ExoDatabaseProvider
+import com.google.android.exoplayer2.database.StandaloneDatabaseProvider
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import com.google.android.exoplayer2.upstream.HttpDataSource
+import com.google.android.exoplayer2.upstream.RawResourceDataSource
+import com.google.android.exoplayer2.upstream.cache.CacheDataSource
+import com.google.android.exoplayer2.upstream.cache.LeastRecentlyUsedCacheEvictor
+import com.google.android.exoplayer2.upstream.cache.SimpleCache
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding : ActivityMainBinding by lazy {
+    private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(LayoutInflater.from(this))
     }
+
+    private lateinit var exoPlayer: ExoPlayer
+
+    private lateinit var cacheDataSourceFactory : CacheDataSource.Factory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        initCacheSetting()
+        initPlayer()
+
+    }
+
+    private fun initPlayer() {
+        exoPlayer = ExoPlayer.Builder(this@MainActivity).setMediaSourceFactory(DefaultMediaSourceFactory(this@MainActivity).setDataSourceFactory(cacheDataSourceFactory)).build()
+        with(exoPlayer) {
+            binding.playerView.player = this
+            setMediaItem(MediaItem.fromUri("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"))
+            playWhenReady = true
+            prepare()
+        }
+
+    }
+
+
+    private fun initCacheSetting(){
+        val cacheEvictor = LeastRecentlyUsedCacheEvictor((100 * 1024 * 1024).toLong())
+        val httpDataSourceFactory = DefaultHttpDataSource.Factory()
+        val dataBaseProvider = StandaloneDatabaseProvider(this@MainActivity)
+        val simpleCache = SimpleCache(File(cacheDir, "media"), cacheEvictor, dataBaseProvider)
+        cacheDataSourceFactory = CacheDataSource.Factory().setCache(simpleCache).setUpstreamDataSourceFactory(httpDataSourceFactory)
     }
 
 }
